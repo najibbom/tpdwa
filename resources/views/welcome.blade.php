7fbf79563a665c908e105531551e96d8b2138a16
Najib Boubrim <boubrimnadjib@Najibs-MacBook-Pro.local>
2020-10-16 18:10:09 +0100

    Added authentication to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @auth
+                        Bonjour, {{ Auth::user()->name }} {{ Auth::user()->lastname }}
+                        <br>
+                        Vous êtes toujours connecté!
+                    @else
+                        TP <br>Developpement Web Avancée
+                    @endauth
                 </div>
             </div>
         </div>
